c548559501b328b24758f70083dfe8883e10ee65
Omar Mohamed <omarcoptan9@gmail.com>
2026-06-26 19:40:19 +0300

    feat(packages): add some essential packages
    Change-Id: Iae2e9c8493bb1db7143d518c0dbf65c9f6d6bc8f

--- modules/nixos/packages.nix
+++ modules/nixos/packages.nix
@@ -6,9 +6,21 @@
   programs.zsh.enable = true;
 
   environment.systemPackages = with pkgs; [
+	btop
+	git
+	bat
+	git-filter-repo
+	git-lfs
+	gh
+	ripgrep
     fastfetch
     firefox
     neovim
+	jq
+	jujutsu
+	tmux
+	tmate
     wget
+	zstd
   ];
 }
