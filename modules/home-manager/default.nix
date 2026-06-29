528dea34bbbbb96226dcff02fc3a7f1cfe92ff60
Omar Mohamed <omarcoptan9@gmail.com>
2026-06-29 15:10:29 +0300

    feat(home-manager): enable xdg.userDirs
    Change-Id: Iab0e7ced113d583b42fa3dac6606a696b63e1282

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1,6 +1,6 @@
 # modules/home-manager/default.nix
 
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 
 {
   imports = [
@@ -13,6 +13,20 @@
   ];
 
   programs.home-manager.enable = true;
+
+  xdg.userDirs = {
+	enable = true;
+
+	createDirectories = true;
+    desktop = "${config.home.homeDirectory}";
+    documents = "${config.home.homeDirectory}/docs";
+	download = "${config.home.homeDirectory}/dls";
+    videos = "${config.home.homeDirectory}/vids";
+    pictures = "${config.home.homeDirectory}/pics";
+    music = "${config.home.homeDirectory}/music";
+    publicShare = "${config.home.homeDirectory}/docs/share";
+    templates = "${config.home.homeDirectory}/docs/templs";
+  };
 
   home.packages = with pkgs; [
 	mpv
