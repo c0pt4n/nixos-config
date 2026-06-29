fbf7ff802e2d9caccb8fce06b6f07d852d1b1820
Omar Mohamed <omarcoptan9@gmail.com>
2026-06-29 16:56:41 +0300

    feat(home-manager): configure bat
    Change-Id: I654ec9939752182445d609db70029ef79e421d03

--- modules/home-manager/packages.nix
+++ modules/home-manager/packages.nix
@@ -7,6 +7,14 @@
 
   programs.discord.enable = true;
 
+  programs.bat = {
+	enable = true;
+	config = {
+	  tabs = "4";
+	  theme = "Nord";
+	};
+  };
+
   home.packages = with pkgs; [
 	telegram-desktop
 	vencord
