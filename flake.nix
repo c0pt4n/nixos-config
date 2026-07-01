ebbd3b8e9a70fe0ef72a5b32bd2a0ea52bfb5423
Omar Mohamed <omarcoptan9@gmail.com>
2026-07-01 03:00:50 +0300

    feat(flake): add stable channel
    Change-Id: Ic7fd8a69f5b16824d77369a293f3c9bb9b78ef74

--- flake.nix
+++ flake.nix
@@ -3,19 +3,27 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+	nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-26.05";
     home-manager = {
       url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs = inputs@{ self, nixpkgs, home-manager, ... }: {
+  outputs = inputs@{ self, nixpkgs, nixpkgs-stable, home-manager, ... }:
+  let
+	system = "x86_64-linux";
+	stable = import nixpkgs-stable {
+	  inherit system;
+	  config.allowUnfree = nixpkgs-stable.lib.mkDefault true;
+	};
+  in {
     nixosConfigurations.virtbox = nixpkgs.lib.nixosSystem {
       modules = [
 		./modules
         ./hosts/virtbox
 		home-manager.nixosModules.home-manager
       ];
-      specialArgs = { inherit inputs; };
-    };
+      specialArgs = { inherit inputs stable; };
+	};
   };
 }
