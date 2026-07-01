63ef56faebf255e849673c715d49840732219c73
Omar Mohamed <omarcoptan9@gmail.com>
2026-07-01 03:04:25 +0300

    feat(desktop): add initial desktop config
    Change-Id: I8dc8286e10a3670552869ea5f86962fbc2c263c9

--- modules/nixos/desktop.nix
+++ modules/nixos/desktop.nix
@@ -1,8 +1,40 @@
 # modules/nixos/desktop.nix
 
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
 {
   # Enable NetworkManager
   networking.networkmanager.enable = true;
+
+  programs.mangowc = {
+	enable = true;
+  };
+
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+
+  services.displayManager.ly = {
+	enable = true;
+	x11Support = false;
+	settings = {
+	  animation = "matrix";
+	};
+  };
+
+  security.polkit.enable = true;
+  services.gnome.gnome-keyring.enable = true;
+  security.pam.services.swaylock = { };
+
+  # Enable udisks2 service
+  services.udisks2.enable = true;
+
+  environment.systemPackages = with pkgs; [
+	foot
+	waybar
+	mako
+	bluetui
+	swayidle
+	wl-clipboard
+	waylock
+	wbg
+  ];
 }
