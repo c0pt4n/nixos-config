c8f26d2abba9693e1cb15a40deb61f79fee54472
Omar Mohamed <omarcoptan9@gmail.com>
2026-07-01 03:36:19 +0300

    feat(security): add some security tools
    Change-Id: I5a00a5921bda38554cd4fd54015ed481759be691

--- modules/nixos/security.nix
+++ modules/nixos/security.nix
@@ -1,22 +1,29 @@
 # modules/nixos/security.nix
 
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
 {
   programs.wireshark.enable = true;
   users.users.omar.extraGroups = [ "wireshark" ];
 
   environment.systemPackages = with pkgs; [
+	ffuf
 	frida-tools
 	netdiscover
 	bloodhound-ce
 	rusthound-ce
 	smbmap
 	metasploit
+	masscan
+	thc-hydra
+	gobuster
 	aircrack-ng
+	nmap
 	jadx
 	apktool
 	ghidra
+	sqlmap
+	subfinder
 	hashcat
 	hashcat-utils
   ];
